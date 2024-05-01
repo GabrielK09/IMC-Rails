f9f21b43993501f0c9d2af729d1f507bc46e0627
TsukiKtk <87103106+TsukiKtk@users.noreply.github.com>
2024-05-01 16:24:11 -0300

    Finish

--- app/controllers/imc_area_controller.rb
+++ app/controllers/imc_area_controller.rb
@@ -5,6 +5,33 @@ class ImcAreaController < ApplicationController
 
   end
 
+  def very_low_weight
+
+  end
+
+  def low_weight
+
+  end
+
+  def normal_weight
+
+  end
+
+  def overweight
+
+  end
+
+  def obesityI
+  end
+
+  def obesityII
+  end
+
+  def obesityIII
+    #
+  end
+
+
   def calculo
     @height = params[:height].to_f # altura
     @weight = params[:weight].to_f # peso
@@ -14,37 +41,37 @@ class ImcAreaController < ApplicationController
       imc = imc.ceil(1)
 
       if imc < 16.9
-        redirect_to imc_area_resultado_path
+        redirect_to  imc_area_very_low_weight_path
         flash[:imc] = "Seu imc é de: #{imc}"
         flash[:situacao] = "Você está muito abaixo do peso!"
 
       elsif imc > 17 && imc < 18.4
-        redirect_to imc_area_resultado_path
+        redirect_to imc_area_low_weight_path
         flash[:imc] = "Seu imc é de: #{imc}"
         flash[:situacao] = "Você está abaixo do peso!"
 
       elsif imc > 18.5 && imc < 24.9
-        redirect_to imc_area_resultado_path
+        redirect_to imc_area_normal_weight_path
         flash[:imc] = "Seu imc é de: #{imc}"
         flash[:situacao] = "Você está com peso normal!"
 
       elsif imc > 25 && imc < 29.9
-        redirect_to imc_area_resultado_path
+        redirect_to imc_area_overweight_path
         flash[:imc] = "Seu imc é de: #{imc}"
         flash[:situacao] = "Você está acima do peso!"
 
       elsif imc > 30 && imc < 34.9
-        redirect_to imc_area_resultado_path
+        redirect_to imc_area_obesityI_path
         flash[:imc] = "Seu imc é de: #{imc}"
         flash[:situacao] = "Obesidade grau I!"
 
       elsif imc > 35 && imc < 40
-        redirect_to imc_area_resultado_path
+        redirect_to imc_area_obesityII_path
         flash[:imc] = "Seu imc é de: #{imc}"
         flash[:situacao] = "Obesidade grau II!"
 
       elsif imc > 40
-        redirect_to imc_area_resultado_path
+        redirect_to imc_area_obesityIII_path
         flash[:imc] = "Seu imc é de: #{imc}"
         flash[:situacao] = "Obesidade grau III!"
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   get 'imc_area/calculo'
   get 'imc_area/resultado'
 
+  get 'imc_area/very_low_weight'
+  get 'imc_area/low_weight'
+  get 'imc_area/normal_weight'
+  get 'imc_area/overweight'
+  get 'imc_area/obesityI'
+  get 'imc_area/obesityII'
+  get 'imc_area/obesityIII'
+
   get '/calculo', to:'imc_area#index'
   post '/calculo', to: 'imc_area#calculo'
 
